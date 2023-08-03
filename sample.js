5bd4673d3000b3007864a4186e1c76cff1678875
Sarvesh <sarveshrajendran512@gmail.com>
2023-08-03 15:09:20 +0530

    color

---
+++ sample.js
@@ -0,0 +1,34 @@
+var a = 120
+console.log(a)
+{
+    let b = 30
+    console.log(b)
+}
+const c = 23
+console.log(c)
+
+var a = 0
+if(a==0){
+    console.log("it is zero")
+}
+
+else if(a>0){
+    console.log("the number is postive")
+}
+
+else{
+    console.log("the number is negative")
+}
+a= 0
+i=0
+while(i<0){
+    commute.log(i)
+    i++
+}
+a= 3
+for(i=0;i<a;i++){
+    console.log(i)
+}
+
+var array=[2,4,8,16,32,64,128,256,512,1024,2040]
+array.forEach(element => console.log(element))
