e0e24380b2bc8409f8fbe7e2c071f15b90db96b1
Sarvesh <sarveshrajendran512@gmail.com>
2023-08-04 09:33:34 +0530

    Files added

--- sample.js
+++ sample.js
@@ -32,3 +32,17 @@ for(i=0;i<a;i++){
 
 var array=[2,4,8,16,32,64,128,256,512,1024,2040]
 array.forEach(element => console.log(element))
+function first(){
+    document.getElementById("hello").innerHTML= "I am in First Year"
+}
+function second(){
+    document.getElementById("hello").innerHTML= "I am in Second Year"
+}
+
+function third(){
+    document.getElementById("hello").innerHTML= "I am in Third Year"
+}
+
+function fourth(){
+    document.getElementById("hello").innerHTML= "I am in Fourth Year"
+}
